684013efb4940f0f0275cf28eaa123636f943b16
Ricardo Araújo Paes <38794747+ricardoapaes@users.noreply.github.com>
2026-04-17 16:13:16 -0300

    ✨ [EIC-701] Atualizar EiChefe Backend para PHP 8.x (#4)

--- src/Console.php
+++ src/Console.php
@@ -5,12 +5,15 @@ namespace Like\Codeception;
 use Codeception\Event\FailEvent;
 use Codeception\Subscriber\Console as SubscriberConsole;
 
-class Console extends SubscriberConsole
-{
-    public function printFail(FailEvent $e)
-    {
-        parent::printFail($e);
-    }
+class Console extends SubscriberConsole {
+	public function printFail(FailEvent $event, ?int $eventNumber = null): void {
+		$method = new \ReflectionMethod(SubscriberConsole::class, 'printFail');
 
-    
+		$args = [$event];
+		if ($method->getNumberOfParameters() > 1 && $eventNumber !== null) {
+			$args[] = $eventNumber;
+		}
+
+		$method->invokeArgs($this, $args);
+	}
 }
--- src/StackTrace.php
+++ src/StackTrace.php
@@ -4,50 +4,56 @@ namespace Like\Codeception;
 
 use Codeception\Lib\Console\Message;
 
-trait StackTrace
-{
-    public function getExceptionTrace($e)
-    {
-        $lines = [];
+trait StackTrace {
+	public function getExceptionTrace($e) {
+		$lines = [];
 
-        if ($e instanceof \PHPUnit\Framework\SkippedTestCase or $e instanceof \PHPUnit\Framework\IncompleteTestCase) {
-            return;
-        }
+		if ($this->isSkippedOrIncomplete($e)) {
+			return;
+		}
 
-        $trace = \PHPUnit\Util\Filter::getFilteredStacktrace($e, false);
+		$trace = \PHPUnit\Util\Filter::getFilteredStacktrace($e);
 
-        $i = 0;
-        foreach ($trace as $step) {
-            $i++;
+		$i = 0;
+		foreach ($trace as $step) {
+			$i++;
 
-            $message = $this->message($i)->prepend('#')->width(4);
+			$message = $this->message($i)->prepend('#')->width(4);
 
-            if (! isset($step['file'])) {
-                foreach (['class', 'type', 'function'] as $info) {
-                    if (! isset($step[$info])) {
-                        continue;
-                    }
-                    $message->append($step[$info]);
-                }
-                $lines[] = $message->getMessage();
-                continue;
-            }
-            $message->append($step['file'] . ':' . $step['line']);
-            $lines[] = $message->getMessage();
-        }
+			if (! isset($step['file'])) {
+				foreach (['class', 'type', 'function'] as $info) {
+					if (! isset($step[$info])) {
+						continue;
+					}
+					$message->append($step[$info]);
+				}
+				$lines[] = $message->getMessage();
+				continue;
+			}
+			$message->append($step['file'] . ':' . $step['line']);
+			$lines[] = $message->getMessage();
+		}
 
-        if (method_exists($e, 'getPrevious')) {
-            $prev = $e->getPrevious();
-            if ($prev) {
-                $lines += $this->printExceptionTrace($prev);
-            }
-        }
+		if (method_exists($e, 'getPrevious')) {
+			$prev = $e->getPrevious();
+			if ($prev) {
+				$lines += $this->getExceptionTrace($prev);
+			}
+		}
 
-        return $lines;
-    }
+		return $lines;
+	}
 
-    private function message($message)
-    {
-        return new Message($message);
-    }
+	private function message($message) {
+		return new Message($message);
+	}
+
+	private function isSkippedOrIncomplete($e): bool {
+		$phpUnit10Skipped = class_exists('PHPUnit\\Event\\Test\\Skipped') && $e instanceof \PHPUnit\Event\Test\Skipped;
+		$phpUnit10Incomplete = class_exists('PHPUnit\\Event\\Test\\MarkedIncomplete') && $e instanceof \PHPUnit\Event\Test\MarkedIncomplete;
+		$legacySkipped = class_exists('PHPUnit\\Framework\\SkippedTest') && $e instanceof \PHPUnit\Framework\SkippedTest;
+		$legacyIncomplete = class_exists('PHPUnit\\Framework\\IncompleteTest') && $e instanceof \PHPUnit\Framework\IncompleteTest;
+
+		return $phpUnit10Skipped || $phpUnit10Incomplete || $legacySkipped || $legacyIncomplete;
+	}
 }
